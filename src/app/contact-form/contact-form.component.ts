d0a2f555b93b21ed7c1e99c7acb63a85abe8a291
Luis Alexander Parra Dita <alexanderpd.dev@gmail.com>
2024-11-30 13:26:52 -0500

    Implementacion de botones editar y eliminir, no funcinales.

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,6 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { ContactService } from '../services/contact.service';
 
 @Component({
@@ -10,10 +10,23 @@ import { ContactService } from '../services/contact.service';
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
-export default class ContactFormComponent {
+export default class ContactFormComponent implements OnInit{
   private fb = inject(FormBuilder);
   private router = inject(Router);
+  private route = inject(ActivatedRoute);
   private contactService = inject(ContactService);
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    
+    if (id) {
+      this.contactService.get(parseInt(id))
+      .subscribe(contact => {
+        console.log('c', contact);
+      })
+    }   // min: 2:12:40
+
+  }
 
   form = this.fb.group({
     name: ['', [Validators.required]],
